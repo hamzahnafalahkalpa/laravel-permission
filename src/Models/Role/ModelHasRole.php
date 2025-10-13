7881de38a268448e75e250504bead34aeb1b05ed
hamzah <hamzahnafalah@gmail.com>
2025-10-13 12:44:25 +0700

    add soap

--- src/Models/Role/ModelHasRole.php
+++ src/Models/Role/ModelHasRole.php
@@ -4,7 +4,6 @@ namespace Hanafalah\LaravelPermission\Models\Role;
 
 use Hanafalah\LaravelHasProps\Concerns\HasCurrent;
 use Hanafalah\LaravelSupport\Models\BaseModel;
-use Illuminate\Database\Eloquent\Concerns\HasUlids;
 
 class ModelHasRole extends BaseModel
 {
